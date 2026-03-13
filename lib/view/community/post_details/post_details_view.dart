57902706eee1c695a00823b99ba913c79461b6c7
mdtusher2025 <mdtusher.bdcalling@gmail.com>
2026-03-13 14:48:22 +0600

    report added

--- lib/view/community/post_details/post_details_view.dart
+++ lib/view/community/post_details/post_details_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
 import 'package:training_plus/core/utils/colors.dart';
 import 'package:training_plus/core/utils/extention.dart';
 import 'package:training_plus/core/utils/helper.dart';
